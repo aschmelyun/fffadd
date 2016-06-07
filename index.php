200d1d35aa2853f168e99146ef8b84495c35b42f
Andrew Schmelyun <schmelyun@gmail.com>
2016-06-07 03:47:52 -0400

    Added introduction area if no colors present

--- index.php
+++ index.php
@@ -14,17 +14,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <script type="text/javascript" src="/assets/js/jquery-1.11.3.min.js"></script>
 </head>
-<body>
-    <div id="palette">
-        <?php foreach($colors as $color): ?>
-            <div class="color" style="background:#<?php echo $color;?>" data-color="<?php echo $color;?>">
-                <span>
-                    <em><?php echo '#' . $color;?></em>
-                    <i>Copied</i>
-                </span>
-            </div>
-        <?php endforeach; ?>
-    </div>
+<body class="palette<?php echo (empty($colors) ? ' palette-empty' : null);?>">
+    <?php if( empty($colors) ):?>
+        <?php $example = 'http://fffadd.xyz/' .
+            sprintf('%02x', rand(0,255)) . sprintf('%02x', rand(0,255)) . sprintf('%02x', rand(0,255)) . '/' .
+            sprintf('%02x', rand(0,255)) . sprintf('%02x', rand(0,255)) . sprintf('%02x', rand(0,255)) . '/' .
+            sprintf('%02x', rand(0,255)) . sprintf('%02x', rand(0,255)) . sprintf('%02x', rand(0,255)) . '/' .
+            sprintf('%02x', rand(0,255)) . sprintf('%02x', rand(0,255)) . sprintf('%02x', rand(0,255)) . '/' .
+            sprintf('%02x', rand(0,255)) . sprintf('%02x', rand(0,255)) . sprintf('%02x', rand(0,255)); ?>
+        <div id="intro">
+            <h1>fffadd</h1>
+            <p>Build your own palette by adding in colors using a url structure. For example:</p>
+            <p><a href="<?php echo $example;?>"><?php echo $example;?></a></p>
+        </div>
+    <?php else:?>
+        <div id="palette">
+            <?php foreach($colors as $color): ?>
+                <div class="color" style="background:#<?php echo $color;?>" data-color="<?php echo $color;?>">
+                    <span>
+                        <em><?php echo '#' . $color;?></em>
+                        <i>Copied</i>
+                    </span>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php endif;?>
     <script type="text/javascript" src="/assets/js/main.js"></script>
 </body>
 </html>
